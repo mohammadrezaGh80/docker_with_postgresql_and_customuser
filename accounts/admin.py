78d919b70127fc982214f9d103d6e47c9068054c
Mohammadreza Gharghabi <mohammadreza.gharghabi6@gmail.com>
2023-02-16 12:01:36 +0330

    complete config for customuser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,13 @@
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth import get_user_model
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    model = get_user_model()
+    model = CustomUser
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     list_display = ("username", "email")
